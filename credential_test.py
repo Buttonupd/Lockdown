1e5d17c01f12aa29fdbe351fdc031d34c149541c
Buttonupd <Dankariuki0101@gmail.com>
2020-04-16 12:10:35 +0300

    testing differential credential features for optimization

--- credential_test.py
+++ credential_test.py
@@ -6,3 +6,22 @@ class TestCredential(unittest.TestCase):
 
     def setUp(self):
         self.new_credential = Credential("Button", "up", "Dan")
+
+    def test_init(self):
+        self.assertEqual(self.new_credential.first_name, "Button")
+        self.assertEqual(self.new_credential.last_name, "up")
+        self.assertEqual(self.new_credential.password, "Dan")
+
+    def test_save_credential(self):
+        self.new_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list), 1)
+
+    def test_save_multiple_credential(self):
+        self.new_credential.save_credential()
+        test_credential = Credential("Humans", "are", "Mortal")
+        test_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- credentials.py
+++ credentials.py
@@ -5,3 +5,8 @@ class Credential:
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
+
+    def save_credential(self):
+
+        Credential.credential_list.append(self)
+
